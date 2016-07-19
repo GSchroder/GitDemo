ce189020319fe92539022084985991ca81668a01
Greg Schroder <gregory@entelect.co.za>
2016-07-19 14:02:05 +0200

    Renamed username to name on master

--- GitDemo/GitDemo/MyClass.cs
+++ GitDemo/GitDemo/MyClass.cs
@@ -9,6 +9,6 @@ namespace GitDemo
     public class MyClass
     {
         public int Id { get; set; }
-        public string Username { get; set; }
+        public string Name { get; set; }
     }
 }
